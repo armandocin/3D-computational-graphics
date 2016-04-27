bbbbb8a78c6738cb713b4c694edf9fd5641d36b8
armandocin <armando.cincotti@gmail.com>
2016-04-27 19:53:52 +0200

    modified neuenational.py

--- neuenational.py
+++ neuenational.py
@@ -10,17 +10,16 @@ submodel = STRUCT(MKPOLS((V,EV)))
 ##VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.07))
 
 """ trasform1azione di scala """
-assert EV[77] == (88,141) ##spigolo di un muro del pianoterra. 88 e 141 sono i vertici
-assert V[88],V[141] == ([0.1215, 0.1201], [0.8786, 0.1201])
-## lo spigolo 77 è lungo 0.8786-0.1215 = 0.7536 e deve diventare di 50.4. Il fattore di scala è quindi 50.4/0.7536=66.88
-V = ((mat(V) - [V[8][0],V[4][1]]) * (50.4/0.7536)).tolist() ##voglio allineare i pilastri in modo corretto e i vertici 8 e 4 sono quelli che prendo per trovare il vettore di traslazione su x e y
+assert EV[32] == (30,69) ##spigolo di un muro del pianoterra. 30 e 69 sono i vertici
+assert V[30],V[69] == ([7.146513749511273, 7.251609539945262], [57.54651374951127, 7.251609539945262])
+V = ((mat(V) - [V[111][0],V[46][1]]) * (50.3/(V[69][0]-V[30][0]))).tolist() ##voglio allineare i pilastri in modo corretto e i vertici 8 e 4 sono quelli che prendo per trovare il vettore di traslazione su x e y
 
 """ Divido i muri di altezza diversa """
-stairsEdges = [106,53,41,25,60,51,58,94,115,40,71,92,43,131,134,133,19,75] ##spigoli elle ringhiere delle scale
+stairsEdges = [4,8,3,84,51,136,73,21,123,116,87,113,5,125,27,127,2,107] ##spigoli elle ringhiere delle scale
 lines_stairs = [[V[EV[e][0]],V[EV[e][1]]] for e in stairsEdges]
-ductsEdges = [5,3,107,122,38,50,144,64] ##spigoli dei condotti dell'aria
-panelsEdges = [82,147,28,65,132,127,104,9,4,12,24,54,14,45,21,129,143,111,83,105,73,72,89,16,23,84] ##spigoli dei pannelli
-wallsEdges = [26,119,79,77]
+ductsEdges = [91,25,78,126,24,26,95,89] ##spigoli dei condotti dell'aria
+panelsEdges = [63,10,47,40,58,131,9,17,75,142,132,59,129,114,12,94,33,104,151,46,39,14,134,64,77,147] ##spigoli dei pannelli
+wallsEdges = [32,61,106,45]
 pillarsEdges = set(range(len(EV))).difference(panelsEdges+ductsEdges+stairsEdges+wallsEdges)
 
 """ Faccio l'OFFSET """
@@ -29,7 +28,7 @@ stairsHPCs = STRUCT(AA(POLYLINE)(lines_stairs))
 ductsHPCs = STRUCT(AA(POLYLINE)([[V[EV[e][0]],V[EV[e][1]]] for e in ductsEdges]))
 panelsHPCs = STRUCT(AA(POLYLINE)([[V[EV[e][0]],V[EV[e][1]]] for e in panelsEdges]))
 wallsHPCs = STRUCT(AA(POLYLINE)([[V[EV[e][0]],V[EV[e][1]]] for e in wallsEdges]))
-pillars = COLOR(BLACK)(OFFSET([.2,.2])(pillarsHPCs))
+pillars = COLOR(BLACK)(OFFSET([.25,.25])(pillarsHPCs))
 stairs = COLOR(RED)(OFFSET([.025,.025])(stairsHPCs))
 ducts = COLOR(GREEN)(OFFSET([.5,.5])(ductsHPCs))
 panels = COLOR(YELLOW)(OFFSET([.25,.25])(panelsHPCs))
@@ -50,62 +49,80 @@ H,EH = lines2lar(lines)
 HH = AA(LIST)(range(len(H)))
 ##VIEW(larModelNumbering(1,1,1)(H,[HH,EH],STRUCT(MKPOLS([H,EH])),0.1)) 
 H = (mat(H)-H[72]).tolist()
-sx = 50.6/H[54][0]; sy = 8.4/H[54][1]
+sx = 50.4/H[54][0]; sy = 8.4/H[54][1]
 scaling = mat([[sx,0,0],[0,sy,0]])
 H = ( mat(H)*scaling ).tolist()
 
 telaioHPC = STRUCT(MKPOLS([H,EH]))
 telaioN = OFFSET([0.1,0.2,0.05])(telaioHPC)
 telaioN = R([2,3])(PI/2)(telaioN) ##mi sposto su x,z in modo da "estrudere" su y
-telaioS = telaioE = telaioO = telaioN
+telaioS = telaioO = telaioN
 telaioS = T([1,2])([0.05,50.7])(telaioS)
 telaioO = R([1,2])(PI/2)(telaioO)
-telaioE = T([1,2])([50.7,-0.05])(R([1,2])(PI/2)(telaioE))
+
+lines = lines2lines("telaiopt2.lines")
+H,EH = lines2lar(lines)
+HH = AA(LIST)(range(len(H)))
+##VIEW(larModelNumbering(1,1,1)(H,[HH,EH],STRUCT(MKPOLS([H,EH])),0.1)) 
+H = (mat(H)-H[84]).tolist()
+sx = 50.4/H[65][0]; sy = 8.4/H[65][1]
+scaling = mat([[sx,0,0],[0,sy,0]])
+H = ( mat(H)*scaling ).tolist()
+
+doorsEdges = [41,33,39,13,67,4,110,27,123,38]
+doorsHPC = STRUCT(MKPOLS([H,[EH[i] for i in doorsEdges]]))
+talaioEdges = set(range(len(EH))).difference(doorsEdges)
+telaioHPC = STRUCT(AA(POLYLINE)([[H[EH[e][0]],H[EH[e][1]]] for e in talaioEdges]))
+doors = OFFSET([0.2,0.25,0.05])(doorsHPC)
+telaioE = OFFSET([0.1,0.2,0.05])(telaioHPC)
+telaioE = STRUCT([doors,telaioE])
+telaioE = R([2,3])(PI/2)(telaioE)
+telaioE = T([1,2])([50.5,-0.05])(R([1,2])(PI/2)(telaioE))
 
 telaio = COLOR(BLACK)(STRUCT([telaioN,telaioE,telaioO,telaioS]))
-telaio = T([1,2])(V[88])(telaio)
+telaio = T([1,2])(V[30])(telaio)
 
 """ Costruzione della struttra di travi del tetto tramite pyplasm """
 Y,FY = larCuboids([18,18])
 Y,EY = larCuboidsFacets([Y,FY])
-Y=(mat(Y)*3.63).tolist()
-Y=(mat(Y)+[.285,.285]).tolist()
+Y=(mat(Y)*3.577).tolist()
+Y=(mat(Y)+[.107,.107]).tolist()
 #VIEW(STRUCT(MKPOLS([Y,EY])))
-beamsGrid = OFFSET([.3,.3])(STRUCT(MKPOLS([Y,EY])))
+beamsGrid = OFFSET([.25,.25])(STRUCT(MKPOLS([Y,EY])))
 beamsHPC = PROD([ STRUCT([beamsGrid]), INTERVALS(1.8)(1) ])
-beamsHPC = T(3)(8.45)(beamsHPC)
+beamsHPC = T(3)(8.65)(beamsHPC)
 #VIEW(beamsHPC)
 
 Y,FY = larCuboids([18,18])
 Y,EY = larCuboidsFacets([Y,FY])
-Y=(mat(Y)*3.67).tolist()
+Y=(mat(Y)*3.597).tolist()
 ##YY = AA(LIST)(range(len(Y)))
 ##VIEW(larModelNumbering(1,1,1)(Y,[YY,EY],STRUCT(MKPOLS([Y,EY])),1))
 d = [1,38,75,112,149,186,223,260,297,334,371,408,445,482,519,556,593,630,628,591,554,517,480,443,406,369,332,295,258,221,184,147,110,73]
 thinEdges = set(range(len(EY))).difference(d+[i for i in range(37) if i%2==0]+[i for i in range(665,684)])
 thinHPC = STRUCT(AA(POLYLINE)([[Y[EY[e][0]],Y[EY[e][1]]] for e in thinEdges]))
-thinGrid = OFFSET([.15,.15])(thinHPC)
-thin = COLOR(BLACK)(PROD([ STRUCT([thinGrid]), INTERVALS(1.8)(1) ]))
-thin = T(3)(8.45)(thin)
+thinGrid = OFFSET([.1,.1])(thinHPC)
+thin = COLOR(GRAY)(PROD([ STRUCT([thinGrid]), INTERVALS(1.8)(1) ]))
+thin = T(3)(8.65)(thin)
 
 """ Costruzione della copertura del tetto """
 C,FC = larCuboids([1,1])
-C = (mat(C)*66.21).tolist()
-cover = larModelProduct([[C,FC],larQuote1D([.05])])
+C = (mat(C)*64.85).tolist()
+cover = larModelProduct([[C,FC],larQuote1D([.1])])
 C,FC = cover
-CT = (mat(C)+[0,0,10.25]).tolist() ##lo traslo di 10.25 ovvero 8.4 delle mura + 1.85 lo s spessore del muro
-coverHPC = COLOR(BLACK)(STRUCT(MKPOLS([CT,FC])))
+CT = (mat(C)+[0,0,10.45]).tolist() ##lo traslo di 10.25 ovvero 8.4 delle mura + 1.85 lo s spessore del muro
+coverHPC = COLOR(GRAY)(STRUCT(MKPOLS([CT,FC])))
 #VIEW(coverHPC)
 #VIEW(STRUCT([coverHPC,beamsHPC]))
 
 """ Costruzione della base del tetto """
 B,FB = larCuboids([18,18])
 B,EB = larCuboidsFacets([B,FB])
-B = (mat(B)*3.645).tolist()
-roofBase = OFFSET([.6,.6])(STRUCT(MKPOLS([B,EB])))
-roofBaseHPC = COLOR(BLACK)(PROD([ STRUCT([roofBase]), INTERVALS(.05)(1) ]))
-roofBaseHPC = T(3)(8.4)(roofBaseHPC)
+B = (mat(B)*3.575).tolist()
+roofBase = OFFSET([.5,.5])(STRUCT(MKPOLS([B,EB])))
+roofBaseHPC = COLOR(GRAY)(PROD([ STRUCT([roofBase]), INTERVALS(.05)(1) ]))
+roofBaseHPC = T(3)(8.6)(roofBaseHPC) ##8.4 + l'offset su y del telaio
 roof = STRUCT([roofBaseHPC,coverHPC,beamsHPC,thin])
 
-pianoterra = STRUCT([pillarsE,panelsE, telaio, ductsE, stairsE])
+pianoterra = STRUCT([pillarsE,panelsE, telaio, ductsE, stairsE,roof])
 VIEW(pianoterra)
